e9b1495f72a22460f36b47e9ba3b5ac5abbcd4fc
Patricio Almada <patricioalmada911@gmail.com>
2023-04-27 06:59:34 -0300

    Separated components

--- src/config/microcemento.ts
+++ src/config/microcemento.ts
@@ -1,15 +1,16 @@
 export const jobDynamic = [
   {params: { jobs: '1' }},
   {params: { jobs: '2' }},
+  {params: { jobs: '3' }},
 ]
 export const jobConfig = [
     {
       "jobTotalImages": 43
     },
     {
-      "jobTotalImages": 6
+      "jobTotalImages": 43
     },
     {
-      "jobTotalImages": 6
+      "jobTotalImages": 43
     },
 ]
