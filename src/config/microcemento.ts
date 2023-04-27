952901335f7ad138e44ff213b93c794c81bc3348
Patricio Almada <patricioalmada911@gmail.com>
2023-04-27 02:22:29 -0300

    dynamic rutes

---
+++ src/config/microcemento.ts
@@ -0,0 +1,15 @@
+export const jobDynamic = [
+  {params: { jobs: '1' }},
+  {params: { jobs: '2' }},
+]
+export const jobConfig = [
+    {
+      "jobTotalImages": 43
+    },
+    {
+      "jobTotalImages": 6
+    },
+    {
+      "jobTotalImages": 6
+    },
+]
